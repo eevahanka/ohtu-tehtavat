c159592bf430fc06ce8b2540f2774d38e9ca12d4
eevahanka <eeva.hanka@iki.fi>
2021-11-19 17:06:06 +0200

    t1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,27 @@
+from player import Player
+import requests
+
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict
+        )
+
+        players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
---
+++ viikko3/nhl-reader/src/player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self, dict):
+
+        self.name = dict["name"]
+        self.nationality = dict["nationality"]
+        self.assists = dict["assists"]
+        self.goals = dict["goals"]
+        self.penalties = dict["penalties"]
+        self.team = dict["team"]
+        self.games = dict["games"]
+
+#{'name': 'Travis Zajac', 'nationality': 'CAN', 'assists': 12, 'goals': 8, 'penalties': 6, 'team': 'NYI', 'games': 46
+#Sami Vatanen team CAR  goals 5 assists 18
+    def __str__(self):
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+
+
